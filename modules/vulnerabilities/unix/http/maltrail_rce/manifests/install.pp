ccbf41dab13f56199088ecaa5080495ab7af8fd1
rosie <roryjfletch@gmail.com>
2026-02-13 22:08:35 +0000

    Switch to Debian python3-pcapy package for offline support

--- modules/vulnerabilities/unix/http/maltrail_rce/manifests/install.pp
+++ modules/vulnerabilities/unix/http/maltrail_rce/manifests/install.pp
@@ -12,9 +12,6 @@ class maltrail_rce::install {
   # MalTrail tarball name (single file, no splitting needed)
   $tarball = 'maltrail-0.54.tar.gz'
 
-  # pcapy-ng wheel filename (update version as needed)
-  $pcapy_wheel = 'pcapy_ng-0.11.5-py3-none-any.whl'
-
   # Create dedicated user for MalTrail service
   user { $user:
     ensure     => present,
@@ -27,8 +24,8 @@ class maltrail_rce::install {
     ensure => present,
   }
 
-  # Install required packages (no git needed for offline)
-  ensure_packages(['python3', 'python3-pip', 'python3-dev', 'libpcap-dev', 'build-essential'])
+  # Install required packages (including python3-pcapy from Debian repos for offline support)
+  ensure_packages(['python3', 'python3-pip', 'python3-dev', 'libpcap-dev', 'build-essential', 'python3-pcapy'])
 
   # Create user home directory with proper permissions
   file { $user_home:
@@ -37,18 +34,6 @@ class maltrail_rce::install {
     group  => $user,
     mode   => '0750',
     require => User[$user],
-  }
-
-  # OFFLINE: Install pcapy-ng from bundled wheel
-  file { "/tmp/${pcapy_wheel}":
-    ensure => file,
-    source => "puppet:///modules/${modulename}/${pcapy_wheel}",
-  }
-
-  exec { 'install-pcapy-ng':
-    command => "pip3 install /tmp/${pcapy_wheel}",
-    unless  => 'python3 -c "import pcapy" 2>/dev/null',
-    require => File["/tmp/${pcapy_wheel}"],
   }
 
   # OFFLINE: Copy MalTrail tarball
