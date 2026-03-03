4c0019b09fdf59b1a295e2a60768e8077818ec8b
girlier <roryjfletch@Gmail.com>
2026-03-03 15:06:59 +0000

    Add init.pp manifest for Wing FTP RCE module
    - Define class parameters: port, strings_to_leak, leaked_filenames, unix_username
    - Include install, config, service subclasses
    - Establish class ordering with Puppet chaining

---
+++ modules/vulnerabilities/unix/ftp/wingftp_rce/manifests/init.pp
@@ -0,0 +1,14 @@
+class wingftp_rce (
+  $port = undef,
+  $strings_to_leak = [],
+  $leaked_filenames = [],
+  $unix_username = 'ftpuser',
+) {
+  include wingftp_rce::install
+  include wingftp_rce::config
+  include wingftp_rce::service
+
+  Class['wingftp_rce::install']
+    -> Class['wingftp_rce::config']
+    -> Class['wingftp_rce::service']
+}
